0aa2ed1b0b4190984a1b4ebc5610dffd1e832058
anuzza <anuja3580@gmail.com>
2022-10-01 21:19:25 -0500

    callback abstraction challenge

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 const request = require("request");
+const geoCode = require("./utils/geoCode");
+const forecast = require("./utils/forecast");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=a3728fe62d0c3496494bbad3152f9a1b&query=37.8267,-122.4233&units=f";
-request(
-  {
-    url: url,
-    json: true,
-  },
-  (error, response) => {
-    if (error) {
-      console.log("Unable to connect to weather services!");
-    } else if (response.body.error) {
-      console.log("Unable to find the location");
-    } else {
-      console.log(
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike}`
-      );
-    }
-  }
-);
+geoCode("Philadelphia N", (error, data) => {
+  console.log(error);
+  console.log(data);
+});
 
-// Geocoding
-// Address=>Lat/Long=>Weather
-
-// const geocodeUrl =
-//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%Angeles.json?access_token=pk.eyJ1IjoicmlzaGFiaHN0aGEiLCJhIjoiY2txcmE2c2x6MXA4czJ1bzh3aDZqc3UwZSJ9.f_hBTLICQKOvpfNmw8qYIg&limit=1";
-// request(
-//   {
-//     url: geocodeUrl,
-//     json: true,
-//   },
-//   (error, response) => {
-//     if (error) {
-//       console.log("Unable to connect weather services");
-//     } else if (response.body.features.length === 0) {
-//       console.log("Unable to find the location");
-//     } else {
-//       const longitude = response.body.features[0].center[0];
-//       const latitude = response.body.features[0].center[1];
-//       console.log(latitude, longitude);
-//     }
-//   }
-// );
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
