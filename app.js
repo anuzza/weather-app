09dc6681725a37b26ca9741b491bd44e95767658
anuzza <anuja3580@gmail.com>
2022-10-01 19:44:26 -0500

    customizing http request

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=a3728fe62d0c3496494bbad3152f9a1b&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=a3728fe62d0c3496494bbad3152f9a1b&query=37.8267,-122.4233&units=f";
 request(
   {
     url: url,
     json: true,
   },
   (error, response) => {
-    console.log(response.body.current);
+    console.log(
+      `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike}`
+    );
   }
 );
