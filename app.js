9c65ede79528226df6828ba95160daa30738df5c
anuzza <anuja3580@gmail.com>
2022-10-01 20:44:57 -0500

    callback function

--- app.js
+++ app.js
@@ -23,22 +23,22 @@ request(
 // Geocoding
 // Address=>Lat/Long=>Weather
 
-const geocodeUrl =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%Angeles.json?access_token=pk.eyJ1IjoicmlzaGFiaHN0aGEiLCJhIjoiY2txcmE2c2x6MXA4czJ1bzh3aDZqc3UwZSJ9.f_hBTLICQKOvpfNmw8qYIg&limit=1";
-request(
-  {
-    url: geocodeUrl,
-    json: true,
-  },
-  (error, response) => {
-    if (error) {
-      console.log("Unable to connect weather services");
-    } else if (response.body.features.length === 0) {
-      console.log("Unable to find the location");
-    } else {
-      const longitude = response.body.features[0].center[0];
-      const latitude = response.body.features[0].center[1];
-      console.log(latitude, longitude);
-    }
-  }
-);
+// const geocodeUrl =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%Angeles.json?access_token=pk.eyJ1IjoicmlzaGFiaHN0aGEiLCJhIjoiY2txcmE2c2x6MXA4czJ1bzh3aDZqc3UwZSJ9.f_hBTLICQKOvpfNmw8qYIg&limit=1";
+// request(
+//   {
+//     url: geocodeUrl,
+//     json: true,
+//   },
+//   (error, response) => {
+//     if (error) {
+//       console.log("Unable to connect weather services");
+//     } else if (response.body.features.length === 0) {
+//       console.log("Unable to find the location");
+//     } else {
+//       const longitude = response.body.features[0].center[0];
+//       const latitude = response.body.features[0].center[1];
+//       console.log(latitude, longitude);
+//     }
+//   }
+// );
