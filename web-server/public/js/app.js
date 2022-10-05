6d09d859e124867a852b30f3810e72184dc47566
anuzza <anuja3580@gmail.com>
2022-10-04 21:16:19 -0500

    wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,26 +1,33 @@
 console.log("Client side javascript file is loaded");
 
+const msg1 = document.querySelector("#msg1");
+const msg2 = document.querySelector("#msg2");
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
+
+  msg1.textContent = "Loading....";
+  msg2.textContent = "";
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          msg1.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          msg1.textContent = data.location;
+          msg2.textContent = data.forecast;
         }
       });
     }
   );
 });
 
-fetch("http://puzzle.mead.io/puzzle").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
